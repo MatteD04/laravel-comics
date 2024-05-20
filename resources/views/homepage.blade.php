72627853894e4a24e9bb99a57be2b8c99317fd0f
MatteD04 <matteo.dantonio04@gmail.com>
2024-05-20 17:14:17 +0200

    feat: add header and footer

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
+
+
 @section('content')
-    <div class="container">
+    <!-- <div class="container">
         <ul>
             @foreach ($comics as $comic)
                 <li>{{$comic['title']}}</li>
             @endforeach
         </ul>
-    </div>
+    </div> -->
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,66 @@
 
 <body>
     
-    <header>ciao</header>
+    <header>
+        <div class="container">
+            <div class="header-bar">
+                <div class="logo">
+                    <img src="#">
+                </div>
+                <nav class="navigation">
+                    <ul>
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#" class="active">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                        </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
     <main>
         @yield('content')
     </main>
+    <footer>
+        <div class="bg-footer">
+            <div class="container">
+                <div class="content-footer">
+                    <h1>contenuto</h1>
+                    <h1>contenuto</h1>
+                    <h1>contenuto</h1>
+                    <h1>contenuto</h1>
+                    <h1>contenuto</h1>
+                    <h1>contenuto</h1>
+                    <h1>contenuto</h1>
+                    <h1>contenuto</h1>
+                </div>
+            </div>
+        </div>
+    
+        <div class="bg-last-footer">
+            <div class="container footer-info">
+                <div class="sign-up">
+                    <button>SIGN-UP NOW</button>
+                </div>
+                <div class="follow-us">
+                    <h3>FOLLOW US</h3>
+                    <div class="social">
+                        <img src="#">
+                        <img src="#">
+                        <img src="#">
+                        <img src="#">
+                        <img src="#">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 </body>
 
